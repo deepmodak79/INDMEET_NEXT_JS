00787bae8fc174906d366976e217c93ee1897e25
unknown <2020itb008.deep@students.iiests.ac.in>
2024-07-29 01:49:43 +0530

    INDMEET ready v0.1

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,20 @@
+import MeetingTypeList from '@/components/MeetingTypeList';
+
+const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          Where ideas meet action. Let's make every moment count with INDMEET
+      </div>
+
+      <MeetingTypeList />
+    </section>
+  );
+};
+
+export default Home;
