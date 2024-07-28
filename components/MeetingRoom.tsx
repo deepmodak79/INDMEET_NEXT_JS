0c34c39048929be06ab334fb366331c5edc43c03
unknown <2020itb008.deep@students.iiests.ac.in>
2024-07-29 02:25:08 +0530

    upadte MeetingRoom.tsx

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -3,21 +3,19 @@ import { useState } from 'react';
 import {
   CallControls,
   CallParticipantsList,
-  CallStatsButton,
   CallingState,
   PaginatedGridLayout,
   SpeakerLayout,
   useCallStateHooks,
 } from '@stream-io/video-react-sdk';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Users, LayoutList } from 'lucide-react';
+import { Users} from 'lucide-react';
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+  DropdownMenuSeparator, 
 } from './ui/dropdown-menu';
 import Loader from './Loader';
 import EndCallButton from './EndCallButton';
