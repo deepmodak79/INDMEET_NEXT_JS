96ffd0faa8d1f056a283a2113a819147a92b5af6
unknown <2020itb008.deep@students.iiests.ac.in>
2024-07-29 02:36:43 +0530

    updated room.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,12 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  //const now = new Date();
 
-  // const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  // const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          Where ideas meet action. Let's make every moment count with INDMEET
+          Where ideas meet action. Lets make every moment count with INDMEET
       </div>
 
       <MeetingTypeList />
